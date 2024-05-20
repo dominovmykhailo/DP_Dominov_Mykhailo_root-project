259dde6e49ced4491cf39ffc60d81cd75bf2bdce
dominovmykhailo <138981954+dominovmykhailo@users.noreply.github.com>
2024-05-21 02:52:52 +0300

    Новий стан

--- DP_Dominov_Mykhailo_root/urls.py
+++ DP_Dominov_Mykhailo_root/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('home/', views.home),
+    path('', views.home),
+    path('resume/', views.resume),
 ]
